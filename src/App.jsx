b2f7bea2957b33b85ca895dc777c7cfe3db6a462
Pedro Soares <ngrpedro22@gmail.com>
2022-12-16 20:30:42 -0300

    init project + tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="max-w-[900px] m-auto text-center">
+      <div className="">
+        <div className="block my-10 py-8 bg-sky-900 w-full text-white text-3xl">
+          <h1>Expense Tracker</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
