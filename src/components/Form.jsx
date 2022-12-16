4f8bda73ee5eba2691ef812a4e8eae561330d8d8
Pedro Soares <ngrpedro22@gmail.com>
2022-12-16 20:47:06 -0300

    controlled form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,31 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
+  const { register, handleSubmit, resetField } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
       <h1 className="font-semibold text-xl">Transctions</h1>
 
-      <div className="flex flex-col gap-4 my-8">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 my-8 mx-10"
+      >
         <input
           className="border border-gray-300 rounded-md py-2 px-3 text-sm"
           type="text"
           placeholder="Salary, Groceries, Fast-food"
+          {...register("name")}
         />
 
         <select
           className="border border-gray-300 rounded-md py-2 px-3 text-sm"
-          name=""
-          id=""
+          {...register("type")}
         >
           <option value="Investiment">Investiment</option>
           <option value="Expense">Expense</option>
@@ -24,13 +34,13 @@ const Form = () => {
 
         <input
           className="border border-gray-300 rounded-md py-2 px-3 text-sm"
-          type="text"
           placeholder="Amount"
+          {...register("amount")}
         />
         <button className="rounded-md py-3 text-sm font-semibold bg-blue-500 text-white">
           Make transaction
         </button>
-      </div>
+      </form>
     </div>
   );
 };
