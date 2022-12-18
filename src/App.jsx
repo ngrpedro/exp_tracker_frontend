561693df92f8bcb81714e7c0afa4e942c9275202
Pedro Soares <ngrpedro22@gmail.com>
2022-12-18 10:35:23 -0300

    first frontend commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
           <Graph />
+          <p>testando</p>
           <Form />
         </div>
       </div>
